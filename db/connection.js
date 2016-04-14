5196dd7112ec6b1f2e89ea98d4106147c16c3bde
winishasmith <winishasmith@gmail.com>
2016-04-14 15:24:35 -0400

    added connection.js and installed mongoose

---
+++ db/connection.js
@@ -0,0 +1,9 @@
+var mongoose = require("mongoose");
+
+var RecipeSchema = {
+  name: String,
+  description: String
+}
+
+mongoose.model("Recipe", RecipeSchema);
+mongoose.connect("mongodb://localhost/chef_winnie");
--- index.js
+++ index.js
@@ -2,9 +2,12 @@ var express = require("express");
 var app     = express();
 
 app.use("/", express.static("public"));
+// app.use("/js", express.static(__dirname + "/public/js"));
+
 app.get("/", function(req,res){
   res.sendFile(__dirname + "/public/index.html");
 });
+
 
 app.listen(3003, function(){
   console.log("Houston we have lift off!");
