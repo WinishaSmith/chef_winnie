a7d264088ff049065916747a7e2687d8aa067539
winishasmith <winishasmith@gmail.com>
2016-04-14 21:33:06 -0400

    added show view with get and post routes

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 var express = require("express");
 var hbs = require("express-handlebars");
+var parser    = require("body-parser");
 var mongoose = require("./db/connection");
 var app     = express();
 
 var Recipe = mongoose.model("Recipe");
 
+app.use(parser.urlencoded({extended: true}));
 app.set("view engine", "hbs");
 app.engine(".hbs", hbs({
   extname:      ".hbs",
@@ -12,12 +14,27 @@ app.engine(".hbs", hbs({
   layoutsDir:   "views/",
   defaultLayout: "layout-main"
 }));
-app.get("/", function(req,res){
-  // Recipe.find().then(function(){
-  //   res.json(response);
-  // });
 
-  res.render("recipes-index");
+app.get("/", function(req,res){
+  Recipe.find().then(function(response){
+    res.render("recipes-index", {
+      recipes: response
+    });
+  });
+});
+
+app.get("/:name", function(req, res){
+  Recipe.findOne(req.params).then(function(response){
+    res.render("recipes-show", {
+      recipe: response
+    });
+  });
+});
+
+app.post("/:name", function(req, res){
+  Recipe.findOneAndUpdate(req.params, req.body.recipe, {new: true}).then(function(response){
+    res.redirect("/" + response.name);
+  });
 });
 
 
